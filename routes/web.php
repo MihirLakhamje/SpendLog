aaca1d1530838f0576949a9be15774e01357a8da
MihirLakhamje <lakhamjemmihir@gmail.com>
2025-01-26 22:43:02 +0530

    routes for auth added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,8 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register_store');
+
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store'])->name('login_store');
+
+Route::post('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware('auth');
