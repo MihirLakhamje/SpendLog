d3d40f822767d081965db88f1f5e48dae385fde0
MihirLakhamje <lakhamjemmihir@gmail.com>
2025-01-26 20:12:54 +0530

    register controller added

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:8', 'confirmed'],
+        ]);
+
+        if(!Auth::attempt($credentials)) {
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        }
+
+        $user = User::create($credentials);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'You have successfully registered.');
+        
+    }
+}
