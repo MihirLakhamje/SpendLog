85ae90c6f4cfca21a9c688c9a4b47a138e8d559c
MihirLakhamje <lakhamjemmihir@gmail.com>
2025-01-27 09:08:59 +0530

    Google oauth implemented

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+    public function googleLogin()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googleCallback()
+    {
+        try {
+
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('google_id', $googleUser->id)->first();
+
+            if (!$user) {
+                $user = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'google_id' => $googleUser->id,
+                    'google_avatar' => $googleUser->avatar,
+                ]);
+                Auth::login($user);
+                return redirect()->route('users.home');
+            }
+            Auth::login($user);
+            return redirect()->route('users.home');
+        } catch (Exception $e) {
+            dd($e);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\SocialiteController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,12 +10,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
 Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store'])->name('login.store');
 
-Route::post('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware('auth');
+Route::get('/google/redirect', [SocialiteController::class, 'googleLogin'])->name('login.google');
+Route::get('/google/callback', [SocialiteController::class, 'googleCallback'])->name('google.callback');
 
-Route::get('/home', [UserController::class, 'home'])->name('users.home')->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
+    Route::get('/home', [UserController::class, 'home'])->name('users.home');
+});
