cf3d0914be46b118f95c2f215ccf773a62c31f42
MihirLakhamje <lakhamjemmihir@gmail.com>
2025-01-29 09:15:48 +0530

    expense create function done

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,24 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Expense;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ExpenseController extends Controller
 {
     public function index()
     {
-        return view('expenses.index');
+        $expenses = Expense::where('user_id', Auth::id())->simplePaginate(10);
+        dd($expenses);
+        // return view('expenses.index');
     }
 
     public function create()
     {
-        return view('expenses.create');
+        return view('expenses.create', [
+            'categories' => [
+                'Food',
+                'Entertainment',
+                'Transport',
+                'Health',
+                'Clothing',
+                'Travel',
+                'Education',
+                'Miscellaneous'
+            ]
+        ]);
     }
 
-    public function edit()
+    public function edit(Expense $expense)
     {
-        return view('expenses.edit');
+        if (!$expense) {
+            abort(404);
+        }
+        return view('expenses.edit', [
+            'expense' => $expense,
+            'categories' => [
+                'food',
+                'entertainment',
+                'transport',
+                'health',
+                'clothing',
+                'shopping',
+                'travel',
+                'education',
+                'miscellaneous'
+            ]
+        ]);
     }
 
-    
+    public function store(Request $request)
+    {
+        $requestDate = Carbon::parse($request->expense_date)->format('Y-m-d');
+        $request->validate([
+            'category' => ['required', 'string'],
+            'expense_amount' => ['required', 'numeric'],
+            'description' => ['required', 'string'],
+            'expense_date' => ['required', 'date'],
+        ]);
+
+        $category = Category::firstOrCreate([
+            'name' => $request->category,
+            'user_id' => Auth::id(),
+        ]);
+
+        Expense::create([
+            'user_id' => Auth::id(),
+            'category_id' => $category->id,
+            'expense_amount' => $request->expense_amount,
+            'description' => $request->description,
+            'expense_date' => $requestDate,
+        ]);
+
+        return redirect()->route('expenses.index')->with('success', 'Expense created successfully');
+    }
+
+    public function update(Request $request, Expense $expense)
+    {
+        if (!$expense) {
+            abort(404);
+        }
+        $requestDate = Carbon::parse($request->expense_date)->format('Y-m-d');
+        $request->validate([
+            'category' => ['required', 'string'],
+            'expense_amount' => ['required', 'numeric'],
+            'description' => ['required', 'string'],
+            'expense_date' => ['required', 'date'],
+        ]);
+
+        $category = Category::firstOrCreate([
+            'name' => $request->category,
+            'user_id' => Auth::id(),
+        ]);
+
+        $expense->update([
+            'user_id' => Auth::id(),
+            'category_id' => $category->id,
+            'expense_amount' => $request->expense_amount,
+            'description' => $request->description,
+            'expense_date' => $requestDate,
+        ]);
+        return redirect()->route('expenses.index')->with('success', 'Expense updated successfully');
+    }
+
+    public function destroy(Expense $expense)
+    {
+        if (!$expense) {
+            abort(404);
+        }
+        $expense->delete();
+        return redirect()->route('expenses.index')->with('success', 'Expense deleted successfully');
+    }
 }
--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -1,3 +1,57 @@
 <x-layout>
-  <x-slot:title>Add expense | SpentLog</x-slot:title>
+    <x-slot:title>Add expense | SpentLog</x-slot:title>
+    <x-slot:header>
+        Add your expense
+    </x-slot:header>
+
+    <div
+        class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form class="w-full max-w-lg flex flex-col gap-4" action="{{ route('expenses.store') }}" method="POST">
+            @csrf
+
+            <div>
+                <label for="expense_amount"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+
+                <input type="text" name="expense_amount" id="expense_amount"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="e.g. 1000" />
+                <x-form.error name="expense_amount" />
+            </div>
+
+            <div>
+                <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                <select id="category" name="category"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>-- Select a category --</option>
+                    @foreach ($categories as $category)
+                        <option value="{{$category}}">{{ucfirst($category)}}</option>
+                    @endforeach
+                </select>
+                <x-form.error name="category" />
+            </div>
+
+            <div>
+                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description <span><i>(optional)</i></span></label>
+
+                <input type="text" name="description" id="description"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="e.g. I bought a new phone" />
+                <x-form.error name="description" />
+            </div>
+
+            <div>
+                <label for="expense_date"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+
+                <input type="date" name="expense_date" id="expense_date"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
+                <x-form.error name="expense_date" />
+            </div>
+
+            <button type="submit"
+                class="flex self-start text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</button>
+        </form>
+    </div>
+
 </x-layout>
