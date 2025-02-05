84ed97df136c36b36be659e263aa225386bd46c6
MihirLakhamje <lakhamjemmihir@gmail.com>
2025-02-05 16:48:05 +0530

    Updated: ui avatar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -113,8 +113,18 @@
                                 class="sm:hidden flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                                 type="button">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="w-8 h-8 rounded-full" src="{{ auth()->user()->google_avatar }}"
-                                    alt="{{ Auth::user()->name }}">
+                                @if(auth()->user()->google_avatar)
+                                    <img class="w-8 h-8 rounded-full" src="{{ auth()->user()->google_avatar }}"
+                                        alt="{{ Auth::user()->name }}">
+                                @else
+                                    <svg class="w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                @endif
                             </button>
                             <!-- Dropdown menu -->
                             <div id="dropdownAvatar"
@@ -146,8 +156,18 @@
                                 class="sm:flex hidden items-center text-sm pe-1 font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:me-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white"
                                 type="button">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="w-8 h-8 me-2 rounded-full" src="{{ auth()->user()->google_avatar }}"
-                                    alt="{{ Auth::user()->name }}">
+                                @if(auth()->user()->google_avatar)
+                                    <img class="w-8 h-8 me-2 rounded-full" src="{{ auth()->user()->google_avatar }}"
+                                        alt="{{ Auth::user()->name }}">
+                                @else
+                                    <svg class="w-7 h-7 me-1 text-gray-800 dark:text-white" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                @endif
                                 <span class="hidden md:block">{{ Auth::user()->name }}</span>
                                 <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 10 6">
